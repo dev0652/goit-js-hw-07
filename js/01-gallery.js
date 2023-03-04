dee406bac89ab840b36f297529920c82586c1caa
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-03-05 01:31:32 +0200

    tmp

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї
+
+const galleryCnt = document.querySelector('.gallery');
+
+function makeItemMarkup({ preview, original, description }) {
+  return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </div>
+    `;
+}
+
+const galleryMarkup = galleryItems.map(makeItemMarkup).join('');
+
+galleryCnt.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// 2. Реалізація делегування на div.gallery і отримання url великого зображення.
+
+galleryCnt.addEventListener('click', handleImgClick);
+
+function openLargeImg(e) {}
