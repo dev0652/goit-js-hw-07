5e75ddb5f5e6462b9eb22b1b1908c6a46d9eed09
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-03-05 12:23:48 +0200

    Task 2 pre-completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function createGalleryMarkup(arrayOfObjects) {
+  return arrayOfObjects
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <li>
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+        </li>
+         `
+    )
+    .join('');
+}
+
+// Create gallery markup
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+// Insert gallery markup
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// Listen to clicks
+galleryContainer.addEventListener('click', onImgPreviewClick);
+
+function onImgPreviewClick(event) {
+  event.preventDefault();
+  const target = event.target;
+
+  // Check if the clicked area is an image
+  const isImagePreview = target.classList.contains('gallery__image');
+
+  if (!isImagePreview) {
+    return;
+  }
+
+  // Create a SimpleLightbox instance
+  return new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    history: false,
+  });
+}
